f5cb7e94679e946dc9b9b8304bc51bf21e57ad93
Mantram Sharma <sharma.mantram@gmail.com>
2026-02-28 10:06:25 -0800

    Add the reactive total hire and layoff value boxes

--- src/app.py
+++ src/app.py
@@ -58,7 +58,9 @@ app_ui = shiny.ui.page_sidebar(
         output_widget("map"),
     ),
     shiny.ui.layout_columns(
-        shiny.ui.value_box("Hire-Layoff Ratio", shiny.ui.output_text("hire_layoff_ratio"),
+        shiny.ui.value_box("Hire-Layoff Ratio", shiny.ui.output_text("hire_layoff_ratio")),
+        shiny.ui.value_box("Total Hires", shiny.ui.output_text("total_hires")),
+        shiny.ui.value_box("Total Layoffs", shiny.ui.output_text("total_layoffs")),
     ),
     title="Tech Workforce Dashboard"
 )
@@ -115,17 +117,35 @@ def server(input, output, session):
 
     @render.text
     def hire_layoff_ratio():
-        df = filtered_df()
-        if df.empty:
+        filtered_data = filtered_df()
+        if filtered_data.empty:
             return "Hire-Layoff Trend Not Available"
 
-        total_hires = df.loc[:,"new_hires"].sum()
-        total_layoffs = df.loc[:, "layoffs"].sum()
+        total_hires = filtered_data.loc[:,"new_hires"].sum()
+        total_layoffs = filtered_data.loc[:, "layoffs"].sum()
 
         if total_hires == 0 or total_layoffs == 0:
             return "Hire-Layoff Ratio Not Available"
-        else:
-            return f"Hire-Layoff Ratio: {total_hires / total_layoffs:.2f}"
+        
+        return f"Hire-Layoff Ratio: {total_hires / total_layoffs:.2f}"
+    
+    @render.text
+    def total_hires():
+        filtered_data = filtered_df()
+        total_hires = filtered_data.loc[:, "new_hires"].sum()
+        if filtered_data.empty:
+            return "Total Hires Not Available"
+        
+        return f"Total Hires: {total_hires}"
+    
+    @render.text
+    def total_layoffs():
+        filtered_data = filtered_df()
+        total_layoffs = filtered_data.loc[:, "layoffs"].sum()
+        if filtered_data.empty:
+            return "Total Layoffs Not Available"
+        
+        return f"Total Layoffs: {total_layoffs}"
 
 
 app = shiny.App(app_ui, server)
